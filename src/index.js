64aaf50732d7e8482de3ed7beb43320065669dba
Rohit Turkar <rohittur2002@gmail.com>
2022-10-27 08:56:56 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Resturant from './Component/Basic/Resturant';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Resturant/>
+  </React.StrictMode>
+);
+
